21efaf849254de9588b7e72940a104c3e3365343
AfricaMensclub <131932268+AfricaMensclub@users.noreply.github.com>
2023-04-26 20:11:33 -0600

    Add files via upload

---
+++ menu/indexs.php
@@ -0,0 +1,113 @@
+<!doctype html>
+<html lang="es">
+  <head>
+    <!-- Required meta tags -->
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+
+    <!-- Bootstrap CSS -->
+    <link rel="stylesheet" href="css/bootstrap.min.css">
+      <link rel="stylesheet" href="css/estilo.css">
+
+    <title>Hola Mundo</title>
+  </head>
+  <body>
+    
+          <h1 class="">Control de Fichas chicas</h1>
+      
+      <div class="row">
+      <div class="col-sm-3">
+<div class="card" style="width: 18rem;">
+  <img class="card-img-top" src="images/jarra.jpg" alt="Card image cap">
+  <div class="card-body">
+    <h5 class="card-title">Jarra</h5>
+    <p class="card-text">Jarra de whisky.</p>
+         <p class="card-text">Precio $480.</p>
+    <a href="#" class="btn btn-primary">Seleccionar</a>
+  </div>             
+          
+      </div>     
+      
+      
+      </div>          
+          
+<div class="col-sm-3">
+ <div class="card" style="width: 18rem;">
+  <img class="card-img-top" src="images/copag.jpg" alt="Card image cap">
+  <div class="card-body">
+    <h5 class="card-title">Copa Globo</h5>
+    <p class="card-text">Copa Globo.</p>
+         <p class="card-text">Precio $250.</p>
+       <div class="dropdown">
+  <button class="btn btn-secondary dropdown-toggle" type="button" id="dropdownMenuButton" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+    Chicas
+  </button>
+  <div class="dropdown-menu" aria-labelledby="dropdownMenuButton">
+    <a class="dropdown-item" href="#">Teresa</a>
+    <a class="dropdown-item" href="#">Arwen</a>
+      <a class="dropdown-item" href="#">Vanesa</a>
+    <a class="dropdown-item" href="#">Something else here</a>
+  </div>
+</div>
+      <br>
+      
+    <a href="#" class="btn btn-primary">Seleccionar</a>
+  </div>    
+          
+          
+      </div>
+      
+     
+      </div>
+          
+          <div class="col-sm-3">
+ <div class="card" style="width: 18rem;">
+  <img class="card-img-top boones mx-auto d-block " src="images/boones.jpg" alt="Card image cap">
+  <div class="card-body">
+    <h5 class="card-title">Botella Boones Vip</h5>
+    <p class="card-text">Botella Boones</p>
+         <p class="card-text">Precio $1600.</p>
+    <a href="#" class="btn btn-primary">Seleccionar</a>
+  </div>    
+          
+          
+      </div>
+      
+      
+      
+      </div>
+           <div class="col-sm-3">
+ <div class="card" style="width: 18rem;">
+  <img class="card-img-top boones mx-auto d-block " src="images/boones.jpg" alt="Card image cap">
+  <div class="card-body">
+    <h5 class="card-title">Botella Boones Reservado</h5>
+    <p class="card-text">Botella Boones</p>
+         <p class="card-text">Precio $3000.</p>
+    <a href="#" class="btn btn-primary">Seleccionar</a>
+  </div>    
+          
+          
+      </div>
+      
+      
+      
+      </div>
+          
+      </div>
+
+      
+      
+      <div class="alert alert-primary" role="alert">
+  A simple primary alert—check it out!
+</div>
+
+    <!-- Optional JavaScript; choose one of the two! -->
+
+    <!-- Option 1: jQuery and Bootstrap Bundle (includes Popper) -->
+    <script src="js/jquery-3.6.3.min.js"></script> 
+    <script src="js/bootstrap.bundle.min.js" ></script>
+    <!-----  <script src="js/popper.min.js"></script> ----->
+    
+
+  </body>
+</html>
